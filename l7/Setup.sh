46ba6b4de73ab00b40e822c1da64e41cad88cc5f
CodeSyntax{×_×} <rayudmakise@gmail.com>
2024-12-27 01:36:34 +0700

    Add files via upload

---
+++ l7/Setup.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+echo   "l7 Setup"
+read -sp "Press [ENTER] To Start."
+apt update -y
+apt upgrade -y
+apt install screen -y
+apt install htop -y
+apt install python3 -y
+apt install pip -y
+apt install nodejs -y
+apt install npm -y
+apt-get install openjdk-8-jdk
+apt install openjdk-17
+npm install -g npm@10.2.3
+npm i net
+npm i http2
+npm i tls
+npm i cluster
+npm i url
+npm i crypto
+npm i user-agents
+npm i fs
+npm i axios
+npm i https
+npm i header-generator
+npm i cheerio
+npm i gradient-string
+npm i http
+npm i node-fetch
+npm i dns
+npm i util
+npm i hpack
+npm i colors
+npm i os
+npm i socks
+npm i request
+npm i p-limit
+npm i sync-request
+npm i proxy-agent
+npm i fake-useragent
+npm i randomstring
+npm i minimist
+npm i events
+npm i child_process
+npm i cloudscraper
+npm i set-cookie-parser
+npm i random-useragent
+npm i random-ua
+npm i requests
+npm i https-proxy-agent
+npm i crypto-random-string
+npm i hcaptcha-solver
+npm i cloudflare-bypasse
+npm i fingerprint-generator 
+npm i fingerprint-injector 
+npm i playwright-extra 
+npm i zombie
+npm i superagent 
+npm i superagent-proxy
+npm i async 
+npm i ua-parser-js
+npm i await-timeout
+npm i console-log-level
+npm i hpagent
+npm i got
+npm i request-curl
+npm i uuid
+npm i tmp-promise
+npm i playwright-extra
+npm i puppeteer-extra
+npm i sleep-promise
+npm i puppeteer
+npm i puppeteer-extra-plugin-recaptcha
+npm i puppeteer-extra-plugin-adblocker
+npm i puppeteer-extra-plugin-stealth
+npm audit fix
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt-get install ./google-chrome-stable_current_amd64.deb -y
+apt -y install gcc g++ make
+apt install -y gconf-service libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget
+apt-get install -y libgbm-dev

